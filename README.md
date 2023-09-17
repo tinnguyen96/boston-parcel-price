ce98ec3c772fefa7796c3baba1bbb989e8878ae3
Tin Nguyen <tdn@mit.edu>
2023-09-17 18:58:30 -0400

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-Last Update: 09/12/2023
-- This is a readme. 
+# Boston Parcel Price Prediction
+This repo builds a supervised learning model, evaluate accuracy and visualize feature importance, for the task of predicting Boston parcel prices in terms of a parcel's physical attributes.
+
+# Business Use Cases
+Accuracte predictions of a parcel's total value is important to:
+- The City: 
+    - to determine the tax 
+    each year, there are about 160k parcels to price. a good prediction system can reduce the time spent by city officials. 
+For owners/real estate companies
+- which property is improving in value, which is not? 
+    + a couple wants to buy a place, and wants to optimize for future value
+- what is the impact of features/renovations on the total value? 
+    + among a set of possible renovations, which one gives the most value?
+
+# Methodology
+
+## Deciding on Relevant Features 
+Based on https://data.boston.gov/dataset/property-assessment/resource/4973f23e-859e-4190-b308-8223c246147e,
+we propose the following list of features for building predictions the parcel's total value are:
+- living area
+- year built
+- year since remodeling
+
+Future work should include a) number of rooms and b) zipcode as relevant predictive features. 
+
+## Exploratory Data Analysis 
+
+[Basic visualization of the features and the response](assess_property/notebooks/visualize_data.ipynb)
+
+## Building and Evaluating Predictive Model
+[Cross-validation shows that boosting models have higher predictive accuracy compared to linear models](assess_property/notebooks/cross_validate.ipynb)
+
+## Analyze Feature Importance
+[SHAP feature importance plots reveal that LIVING_AREA is the biggest determinant of TOTAL_VALUE](assess_property/notebooks/visualize_feature_importance.ipynb)
--- assess_property/src/assess_property/fit_learner.py
+++ assess_property/src/assess_property/fit_learner.py
@@ -17,10 +17,62 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
 
+from assess_property.read_data import ReadAllYears
+from assess_property.preprocess import RemoveOutlier, ScaleTotalValue
 from assess_property.global_variables import LU_LIST, YR_TO_NAME, ZIPCODE_FILLNA, DATA_DIR
 
-class Experiment:
 
+def MakeExperiment():
+    """ 
+    Experimental configs: 
+    - use all data from 2014 through 2023 
+    - use YR_BUILT, YEAR, YR_REMODEL, LIVING_AREA as features, and predict TOTAL_VALUE
+    - 10-fold CV that treats all rows as iid 
+    """
+
+    # read data from disk 
+    yrs = range(2014,2024)
+    vnames = ["PID", "YR_REMODEL", "YR_BUILT", "LIVING_AREA", "LU", "ZIPCODE", "TOTAL_VALUE","YEAR"]
+    reader = ReadAllYears(yrs, vnames)
+    reader.read_all_years()
+
+    # remove outiers 
+    remover = RemoveOutlier(reader.df)
+    remover.run()
+
+    # scale total value column
+    scaler = ScaleTotalValue(remover.df)
+    scaler.run()
+
+    # select only R1 code 
+    df = scaler.df[scaler.df["LU"] == "R1"].copy()
+
+    # CV experiment 
+    var_names = ["YR_BUILT", 
+                 "YEAR",
+                 "YR_REMODEL",
+                 "LIVING_AREA"]
+    response_name = "TOTAL_VALUE_IN_HUNDRED_GRAND"
+    random_seed = 0
+    n_fold = 10
+    cv_experiment = CV_Experiment(df, 
+                        var_names, 
+                        response_name, 
+                        n_fold=n_fold,
+                        random_seed = random_seed)
+    cv_experiment.set_up()
+
+    onefit_experiment = OneFit_Experiment(df, 
+                        var_names, 
+                        response_name, 
+                        n_fold=n_fold,
+                        random_seed = random_seed)
+    onefit_experiment.set_up()
+
+    return df, onefit_experiment, cv_experiment 
+
+
+class Experiment:
     def __init__(self, 
                  df, 
                  var_names, 
@@ -46,13 +98,6 @@ class Experiment:
         self.ub_ntrain = 2 * self.X.shape[0] # upper bound used to make lgbm determinsitic 
         return 
     
-    def make_cv_folds(self):
-        kf = KFold(n_splits = self.n_fold,
-                   shuffle= True,
-                   random_state= self.random_seed)
-        cv_folds = kf.split(self.X)
-        return cv_folds
-
     def make_pipeline(self,
                       learner):
         # reference on using sklearn pipeline https://scikit-learn.org/stable/modules/compose.html#pipeline
@@ -68,6 +113,48 @@ class Experiment:
                          ('lgbm', LGBMRegressor(subsample_for_bin = self.ub_ntrain))])
             non_learner_steps = [0,1]
         return pipe, non_learner_steps
+
+class OneFit_Experiment(Experiment):
+    def __init__(self,
+                 df, 
+                 var_names, 
+                 response_name,
+                 n_fold,
+                 random_seed):
+         Experiment.__init__(self,
+                            df, 
+                            var_names, 
+                            response_name,
+                            n_fold,
+                            random_seed)
+
+    def fit(self,
+            learner):
+        pipe, _ = self.make_pipeline(learner)
+        pipe.fit(self.X, self.y)
+        return pipe 
+
+class CV_Experiment(Experiment):
+
+    def __init__(self,
+                 df, 
+                 var_names, 
+                 response_name,
+                 n_fold,
+                 random_seed):
+        Experiment.__init__(self,
+                            df, 
+                            var_names, 
+                            response_name,
+                            n_fold,
+                            random_seed)
+    
+    def make_cv_folds(self):
+        kf = KFold(n_splits = self.n_fold,
+                   shuffle= True,
+                   random_state= self.random_seed)
+        cv_folds = kf.split(self.X)
+        return cv_folds
 
     def cv_fit(self,
             learner):
@@ -88,32 +175,8 @@ class Experiment:
                                     scoring= ["neg_mean_absolute_error", 
                                               "neg_root_mean_squared_error",
                                               "neg_mean_absolute_percentage_error"],
+                                    return_estimator=True,
+                                    return_indices = True,
                                     verbose = 1,
                                     n_jobs=4)
         return cv_results
-    
-    def fit(self,
-            learner,
-            fold_idx):
-        """_summary_
-
-        Args:
-            fold_idx (_type_): _description_
-        """
-        cv_folds = list(self.make_cv_folds())
-        train_indices, test_indices = cv_folds[fold_idx]
-        pipe, non_learner_steps = self.make_pipeline(learner)
-
-        xtrain, ytrain = self.X[train_indices], self.y[train_indices]
-        xtest, ytest = self.X[test_indices], self.y[test_indices]
-
-        pipe.fit(xtrain, ytrain)
-        ypred = pipe.predict(xtest)
-
-        datadict = {"xtrain": xtrain,"ytrain": ytrain,
-                    "xtest": xtest, "ytest": ytest}
-        
-        modeldict = {"pipe": pipe, 
-                     "ypred": ypred}
-        
-        return datadict, modeldict  
